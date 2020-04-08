99f7f5c3c4cce26d02df3d087aafb71e0dd0c9c2
EQUENOS-2 <63377939+EQUENOS-2@users.noreply.github.com>
2020-04-09 02:45:33 +0300

    Update db_worker.py

--- box/db_worker.py
+++ box/db_worker.py
@@ -1,5 +1,5 @@
 import pymongo
 from pymongo import MongoClient
 
-app_string = open("box/cluster_app_string.txt", "r").read() #f"{os.environ.get('cluster_app_string')}"
+app_string = f"{os.environ.get('cluster_app_string')}"
 cluster = MongoClient(app_string)
